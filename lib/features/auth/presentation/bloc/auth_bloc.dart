04845637c49d7e93702e0f31d8709eac5e2c2574
raisarahman777 <raisarahman1904@gmail.com>
2025-03-29 17:29:56 +0600

    feat: Add user login functionality

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:blog_app/features/auth/domain/entities/user.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,28 +9,44 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
+  final UserLogin _userLogin;
 
-  AuthBloc({required UserSignUp userSignUp})
+  AuthBloc({required UserSignUp userSignUp, required UserLogin userLogin})
     : _userSignUp = userSignUp,
+      _userLogin = userLogin,
       super(AuthInitial()) {
-    on<AuthSignUp>((event, emit) async {
-      final response = await _userSignUp(
-        UserSignUpParams(
-          name: event.name,
-          email: event.email,
-          password: event.password,
-        ),
-      );
+    on<AuthSignUp>(_onAuthSignUp);
+    on<AuthLogin>(_onAuthLogin);
+  }
 
-      // response.fold(
-      //   (l) => emit(AuthFailure(l.message)),
-      //   (r) => emit(AuthSuccess(r)),
-      // );
+  void _onAuthSignUp(AuthSignUp event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final response = await _userSignUp(
+      UserSignUpParams(
+        name: event.name,
+        email: event.email,
+        password: event.password,
+      ),
+    );
 
-      response.fold(
-        (failure) => emit(AuthFailure(failure.message)),
-        (user) => emit(AuthSuccess(user)),
-      );
-    });
+    // response.fold(
+    //   (l) => emit(AuthFailure(l.message)),
+    //   (r) => emit(AuthSuccess(r)),
+    // );
+
+    response.fold(
+      (failure) => emit(AuthFailure(failure.message)),
+      (user) => emit(AuthSuccess(user)),
+    );
+  }
+
+  void _onAuthLogin(AuthLogin event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+
+    final res = await _userLogin(
+      UserLoginParams(email: event.email, password: event.password),
+    );
+
+    res.fold((l) => emit(AuthFailure(l.message)), (r) => emit(AuthSuccess(r)));
   }
 }
